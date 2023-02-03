26f5a4eef0c2799afef2f331c1220298bd448c62
Dimed <dimedab@gmail.com>
2023-02-03 03:23:26 +0100

    correction

--- Main.sh
+++ Main.sh
@@ -11,8 +11,24 @@ HelpArg "$@"
 
 
 
+if checkFile "$@" ; then 
+    echo "Erreur veuillez donnez 1 fichier correcte"
+    exit 2
+fi
 
+if SpaceArg "$@" ; then 
+    echo "Erreur veuillez donnez 1 seul ou aucun lieu"
+    exit 1
+fi
+if TimeArg "$@" ; then 
+    echo "Erreur veuillez donnez aucune ou un format correcte de date"
+    exit 1
+fi
 
+if TypeArg "$@" ; then 
+    echo "Erreur veuillez donnez au moins 1 type"
+    exit 1
+fi
 
 
 if SortArg "$@" ; then 
